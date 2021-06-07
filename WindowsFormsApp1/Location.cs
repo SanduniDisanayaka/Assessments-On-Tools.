1a5e59706285a9183a3594d08db4f150cc463f29
Sanduni Disanayaka <Made In Sri Lanka@DESKTOP-33MU7I7>
2021-06-07 20:14:13 +0530

    test2

--- WindowsFormsApp1/Location.cs
+++ WindowsFormsApp1/Location.cs
@@ -58,7 +58,7 @@ namespace WindowsFormsApp1
             SqlConnection con = new SqlConnection("Server = tcp:sqlserverml12345.database.windows.net,1433; Initial Catalog = Time_Table_Management_System; Persist Security Info = False; User ID = Millennium; Password = Malinda@12345; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;");
 //test
 //..
-//cvbcv
+//cvbcvv
             {
                 try
                 {
